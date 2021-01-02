43dac466b52a90eec27cfba62337bde1d120beca
scruz <sebastian.cruz@wenance.com>
2021-01-02 12:18:25 -0300

    feat: search screen

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -15,10 +15,20 @@ export class SpotifyService {
   getNewRelease() {
 
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQAwpMlEbn8TzqNnj_6EgSkqRfMEAncbD6xbrvkUh7wBnTZEjp7ybLw4W52xnVQ2p0A5RpB5JqPNZg72_HM'
+      'Authorization': 'Bearer BQCDE7eT8qRWE8nArMEtTD12QYEX7_rzSX_V-CuNC3Taak94k8sfv2MnNPIbd9zLX0bCftGsya8c2Mr6e00'
     })
 
     return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers })
     
+  }
+
+  getArtista(termino: string) {
+    
+    console.log('searching artiasta', termino)
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer BQCDE7eT8qRWE8nArMEtTD12QYEX7_rzSX_V-CuNC3Taak94k8sfv2MnNPIbd9zLX0bCftGsya8c2Mr6e00'
+    })
+
+    return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist`, { headers })
   }
 }
