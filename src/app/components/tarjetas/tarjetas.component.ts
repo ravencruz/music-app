39ba26ef1b92cfc2b86b5f5e029851fbdf80298f
scruz <sebastian.cruz@wenance.com>
2021-01-05 13:50:31 -0300

    feat: router to artist page

--- src/app/components/tarjetas/tarjetas.component.ts
+++ src/app/components/tarjetas/tarjetas.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-tarjetas',
@@ -9,9 +10,26 @@ export class TarjetasComponent implements OnInit {
 
   @Input() items: any[] = []
 
-  constructor() { }
+  constructor(private router: Router) {
+
+  }
 
   ngOnInit(): void {
+  }
+
+
+  verArtista(item: any) {
+    //console.log(item)
+
+    let artistaId = null;
+    if (item.type === 'artist') {
+      artistaId = item.id
+    } else {
+      artistaId = item.artists[0].id
+    }
+
+    console.log(artistaId)
+    this.router.navigate( ['/artist', artistaId] )
   }
 
 }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -17,7 +17,7 @@ export class SpotifyService {
   getQuery( query: string) {
 
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQB3BFn4HtoGm8ElenBeEc2M84TL3PC-gN_y0VmtrjclN8Dxv8Wu2I4pMir4LPitsGEY67VNehuvx7mc4Ok'
+      'Authorization': 'Bearer BQBK2LzjXYnVI5EFNxWMZPOikp1pClpnSGTtbvGzY2EbxnMalgtxNVHkKKqwfqcs04ab9bUqjVDF9W1jnFU'
     })
 
     const url = `https://api.spotify.com/v1/${query}`
